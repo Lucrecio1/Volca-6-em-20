c9be70458b3d4be7f5e40b42652c3f1568f54ae3
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2022-08-17 09:12:14 +0100

    commit

--- crud_volca/index.php
+++ crud_volca/index.php
@@ -2,7 +2,8 @@
 include_once '../includes/header.php'; ?>
 <div class="row">
     <div class="mb-2 container col col-lg-12  p-2 bg-dark">
-        <h1 class=" text-center text-danger">Aqui vai uma imagem</h1>
+        <h1 class=" mb-3 text-center text-danger">Aqui vai uma imagem</h1>
+        <h1 class=" mb-5 text-center text-white-50">info</h1>
         <div class="p-3 bg-primary text-white">.bg-primary</div>
     </div>
 </div>
