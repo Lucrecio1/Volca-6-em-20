9de1e1d60105d739ec8e21108718a79b92509016
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2022-08-17 13:16:27 +0100

    commit

--- crud_volca/index.php
+++ crud_volca/index.php
@@ -1,25 +1,29 @@
 <?php
 include_once '../includes/header.php'; ?>
-<div class="row">
    
-    <body class="container" style="background-image: url('images/bg.jpg')";>
- 
-<!-- Breves informações da aplicação -->
-    <div class="bg-white container">
+<body class="bg-white" >
         <div class="row">
-            <div class="p-4 col-md-12 text-center">
-                <h2 class="site-section-heading text-center font-secondary letraS ah2">1° O QUE É O DESAFIO 6 EM 20</h2>
-                <p class="text-justify alinhar letraS"> de saúde.<br></p>
-
-                <p class="text-justify alinhar  letraS">É o maior evento online e interativo de Angola, focado em
-       melhorar os resultados das pessoas e empresas que trabalham com a internet para
-      fazer negócios (ou divulgar os seus trabalhos). Para você ter uma ideia, são cerca de 70 horas de conteúdos exclusivos e 10
-      palestrantes convidados a sua disposição.
- <br></p>
-
+            <div class="col-lg-12 bg-black mb-3">
+                <h1 class=" text-white-50 text-center">Imagem</h1>
+                <a href="" class=" btn btn-outline-info text-center">Quero me increver no 6 em 20</a>
+            </div>
+            <div class=" text-center">
+                <div class=" col-lg-8 col-md-10 col-sm-12">
+                <h2 class="site-section-heading text-center font-secondary letraS ah2 mb-4">1° O QUE É O DESAFIO 6 EM 20</h2>
+                
+                <p class="text-justify alinhar  letraS mx-3">É o maior evento online e interativo de Angola, focado em
+                melhorar os resultados das pessoas e empresas que trabalham com a internet para
+                fazer negócios (ou divulgar os seus trabalhos). Para você ter uma ideia, são cerca de 70 horas de conteúdos exclusivos e 10
+                palestrantes convidados a sua disposição.
+                </p>
+                </div>
+                <div class="col-lg-4 col-md-2 col-sm-12">
+                    <div >
+                    <h2 class="site-section-heading text-center font-secondary letraS ah2 mb-4">Palestrantes</h2>
+                    </div>
+                </div>
+                
             </div>
         </div>
-
-        <br>
-</div>
+</body>
 <?php include_once '../includes/footer.php';?>
