1736f76373e646803afaffb64ae3fb89fb438869
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2022-08-17 08:23:45 +0100

    commit

--- crud_volca/index.php
+++ crud_volca/index.php
@@ -1,14 +1,4 @@
 <?php
 include_once '../includes/header.php'; ?>
-<div class="d-grid container">
-    <div class="g-md-5 g-sm-5  ">
-    <img src="../img/Como-ser-um-Hacker-2.png" class="img-fluid gap-3" alt="...">
-        
-    </div>
-    <div class="d-grid gap-2 col-8 mx-auto">
-    <button class=" btn-primary p-2">Quero me escrever no 6 em 20 ?</button>
-    </div>
-        <br>
-    
-</div>    
+
 <?php include_once '../includes/footer.php';?>
