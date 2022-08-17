06bfeaa7125f2b2c406f5f677440c9d3ba30ae19
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2022-08-17 10:04:07 +0100

    commit

--- crud_volca/index.php
+++ crud_volca/index.php
@@ -2,9 +2,20 @@
 include_once '../includes/header.php'; ?>
 <div class="row">
     <div class="mb-2 container col col-lg-12  p-2 bg-dark">
-        <h1 class=" mb-3 text-center text-danger">Aqui vai uma imagem</h1>
-        <h1 class=" mb-5 text-center text-white-50">info</h1>
-        <div class="p-3 bg-primary text-white">.bg-primary</div>
+        <h1 class=" mb-3 text-center text-danger">Aqui vai uma imagem </h1>
+        <h1 class=" mb-5 text-center text-white-50">REGINA MÃE DO ADRIEL</h1>
+        <div class=" text-center">
+            <a href="#" class="btn btn-outline-info p-3">QUERO ME INCREVER NO DESAFIO 6 EM 20</a>
+        </div>
+    </div>
+
+    <div class="container mb-2 container col-lg-12  p-2 bg-body">
+        <h1>1° O QUE É O DESAFIO 6 EM 20?</h1>
+        <p class=""> É o maior evento online e interativo de Angola, focado em
+melhorar os resultados das pessoas e empresas que trabalham com a internet para
+fazer negócios (ou divulgar os seus trabalhos). Para você ter uma ideia, são cerca de 70 horas de conteúdos exclusivos e 10
+palestrantes convidados a sua disposição.
+        </p>
     </div>
 </div>
 <?php include_once '../includes/footer.php';?>
