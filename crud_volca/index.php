6d7cecc406f93a8f6cf5ce6a21aa5dd1cf8b1913
Lucrecio Barnabe <lucrecibarnabe@gmail.com>
2022-08-17 09:02:19 +0100

    commit

--- crud_volca/index.php
+++ crud_volca/index.php
@@ -1,4 +1,11 @@
 <?php
 include_once '../includes/header.php'; ?>
+<div class="row">
+    <div class="mb-2 container col col-lg-12  p-2 bg-dark">
+        <h1 class=" text-center text-danger">Aqui vai uma imagem</h1>
+    </div>
 
+    <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+
+</div>
 <?php include_once '../includes/footer.php';?>
